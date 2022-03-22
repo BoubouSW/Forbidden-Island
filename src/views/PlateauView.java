0ab5a7724cf8c891403c037a9a0ae640a7ae03b4
BoubouSW <gilliclement@gmail.com>
2022-03-22 20:07:27 +0100

    improve colors

--- src/views/PlateauView.java
+++ src/views/PlateauView.java
@@ -10,18 +10,18 @@ class PlateauView extends IG.Grille {
 
     public PlateauView(Plateau plat) {
         super(plat.getTaille(), plat.getTaille());
-        this.setBackground(Color.BLUE);
+        this.setBackground(new Color(30,144,255));
         this.p = plat;
         int taille = p.getTaille();
         for (int i = 0; i < taille; i++) {
             for (int j = 0; j < taille; j++) {
                 CaseController cas = new CaseController(plat.getCase(i,j));
                 if (plat.getCase(i,j).getEtat() == Case.Etat.INONDEE)
-                    cas.setBackground(Color.MAGENTA);
+                    cas.setBackground(new Color(95,158,160));
                 else if (plat.getCase(i,j).getEtat() == Case.Etat.SUBMERGEE)
-                    cas.setBackground(Color.BLUE);
+                    cas.setBackground(new Color(30,144,255));
                 else
-                    cas.setBackground(Color.GREEN);
+                    cas.setBackground(new Color(74,160,44));
                 this.ajouteElement(cas);
             }
         }
