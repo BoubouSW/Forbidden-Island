a00985e154b8304a9ac360ddc800ac8976dcccca
PENELOUX Louis-Alexis <loualeOldPC@outlook.com>
2022-03-24 16:19:32 +0100

    m

--- tests/PlayerTest.java
+++ tests/PlayerTest.java
@@ -3,6 +3,7 @@ import junit.framework.TestCase;
 
 public class PlayerTest extends TestCase {
 
+    @Test
     public void testCalculer() throws Exception {
         Plateau plateau = new Plateau(8);
         Player player1 = new Player(plateau,0,"player1",3,3);
