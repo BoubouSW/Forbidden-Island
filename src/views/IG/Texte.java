db36f6b492c5ccaf98e458987678f54a35ecfef8
BoubouSW <gilliclement@gmail.com>
2022-03-27 19:55:06 +0200

    add font perso

--- src/views/IG/Texte.java
+++ src/views/IG/Texte.java
@@ -3,7 +3,8 @@ package IG;
 import javax.swing.JLabel;
 import javax.swing.JComponent;
 import java.awt.*;
-
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Classe pour une zone de texte modifiable.
@@ -19,6 +20,12 @@ public class Texte extends JLabel {
     public Texte(String texte, int texteSize) {
 
         super(texte);
+        /*  SI ON VEUT UNE POLICE PERSO
+        try {
+            Font newFont = Font.createFont(Font.TRUETYPE_FONT,new File("resources/fonts/myfont.ttf")).deriveFont(new Integer(texteSize).floatValue());
+            super.setFont(newFont);
+        }catch (IOException | FontFormatException e) {}
+        */
         super.setFont(new Font("Roboto", Font.PLAIN, texteSize));
         //super.setForeground(Color.RED);
     }
