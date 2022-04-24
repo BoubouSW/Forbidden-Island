6555b3fbd5e349770c2d4c3b1d8e440d459f1e16
BoubouSW <gilliclement@gmail.com>
2022-04-24 23:53:50 +0200

    fix bug test

--- tests/CarteTresorTest.java
+++ tests/CarteTresorTest.java
@@ -7,9 +7,9 @@ public class CarteTresorTest extends TestCase {
     public void test() throws Exception {
         CarteTresor carte = new CarteTresor(CarteTresor.TYPE_CARTE_TRESOR.ARTEF_FEU);
         assertEquals(carte.getValeurCarte(), CarteTresor.TYPE_CARTE_TRESOR.ARTEF_FEU);
-        assertEquals(carte.getId(), 0);
+        assertEquals(carte.getId(), 5);
         CarteTresor carteBis = new CarteTresor(CarteTresor.TYPE_CARTE_TRESOR.CARTE_DE_SABLE);
         assertEquals(carteBis.getValeurCarte(), CarteTresor.TYPE_CARTE_TRESOR.CARTE_DE_SABLE);
-        assertEquals(carteBis.getId(), 1);
+        assertEquals(carteBis.getId(), 6);
     }
 }
